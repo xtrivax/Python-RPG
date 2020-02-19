44c255d41e0bb00909de57492316cd0ca667b07d
xtrivax <xtrivax@gmail.com>
2020-02-19 10:40:53 +0100

    Story file and removel of useless code.

--- Game/story.py
+++ Game/story.py
@@ -1,3 +1,23 @@
+gamestart = """
+        Its dark around you and you feel slightly cold as you wake up. \n
+        You try to pull up your blanket and sleep another round. \n
+        But you grab into the air. There is no blanket you realise. \n
+        You feel the ground around yourself. The ground around is cold and hard \n
+        Dimm light comes from cracks in the ceiling. \n
+        Your eyes adjust slowly to the darkness. \n
+        It seems like you are in some sort of cave. \n
+        You see a small tunnel that seems to lead out of the cave. \n
+         \n
+        You get up slowly and walk along the tunnel.  \n
+        Slowly you go forward while you keep one hand on the cave wall. \n
+        As your eyes adjust a little more you see someone moving further inside the tunnels \n
+
+        You: Hello is someone here? \n
+        
+        The movement stops shortly, then the silhoutte turns to you. \n
+        
+        """
+
 startbattle_init = """
     The Goblin runs towards you. \n
     You feel your Adrenalin rising and you try to;  \n
@@ -6,25 +26,34 @@ startbattle_init = """
     """
 
 startbattle_warrior = """
-        You rush forwards towards the Goblin. \n
+        You rush towards the Goblin. \n
         The surprised Goblin swings his Sword after you. \n
         \n
         But you react fast and evade it. \n
-        You hit the Sword out of his Hand and then kick him. \n
+        You hit the Sword out of his Hand and then kick him in the chest. \n
         He Stumbles. Which gives you enough Time to get his Sword from the Ground. \n
         \n
-        You attack him immediately and stab him into his chest. \n
+        You attack him immediately and stab into his chest. \n
         He cries in Agony while you press him to the ground. \n
         It becomes quiet. You apply some more pressure just to be sure and then pull the sword out. \n
-        You have no scabbard but it should do just fine.
+        Blood flows down the sword and you shortly clean it on the Goblins leather clothes. \n
+        You look at the Sword, its not exactly new but still in good Condition. \n
+        There is no scabbard but it should do just fine.
         """
 
 startbattle_mage = """
-"""
+        You instinctively try to cast a spell.
+        slowly you see that a ball that looks to be made of fire forms in your hand.
+        you can feel the heat that comes from the ball in your hand, it feels like its burning your skin.
+        as the goblin run towards you, you fling the fireball at him.
+        the impact of the fireball knocks the goblin back.
+        he screams in pain as he burns to a crisp.
+
+        """
 
 startbattle_lose = """
         You try think of a way out of this situation but the Goblin is fast. \n
-        You barely had time to react when you allready feel the cold of his Iron Dagger penetrating your Skin.  \n
+        You barely had time to react when you allready feel the cold of his Sword penetrating your Skin.  \n
          \n
         Panic overcomes you and you look around. \n
          \n
